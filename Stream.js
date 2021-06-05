aee165984f27d816d80e0f733638d9dc4f5949e3
Taisuke Fukuno <fukuno@jig.jp>
2021-06-05 22:42:13 +0900

    add once

--- Stream.js
+++ Stream.js
@@ -1,6 +1,7 @@
 class Stream {
   constructor() {
     this.data = {};
+    this.dataonce = {};
     this.autoDestroy = true;
   }
   on(name, f) {
@@ -10,12 +11,26 @@ class Stream {
     } else {
       this.data[name].push(f);
     }
-    console.log("x-on", name, this.data[name]);
+    console.log("on", name, this.data[name]);
+  }
+  once(name, f) {
+    const a = this.dataonce[name];
+    if (!a) {
+      this.dataonce[name] = [f];
+    } else {
+      this.dataonce[name].push(f);
+    }
+    console.log("once", name, this.dataonce[name]);
   }
   emit(name, data) {
     const a = this.data[name];
     if (a) {
       a.forEach(f => f(data));
+    }
+    const aonce = this.dataonce[name];
+    if (aonce) {
+      aonce.forEach(f => f(data));
+      this.dataonce = {};
     }
     console.log("emit", name, data);
   }
